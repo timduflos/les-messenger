42ced1de33039ff742cb49a3fd749f6d48934dde
Timothée Duflos <timothee.duflos@etu.minesparis.psl.eu>
2025-10-28 15:56:39 +0100

    j'ai ajouter channels et users

---
+++ messenger.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+server = {
+    'users': [
+        {'id': 41, 'name': 'Alice'},
+        {'id': 23, 'name': 'Bob'}
+    ],
+    'channels': [
+        {'id': 12, 'name': 'Town square', 'member_ids': [41, 23]}
+    ],
+    'messages': [
+        {
+            'id': 18,
+            'reception_date': datetime.now(),
+            'sender_id': 41,
+            'channel': 12,
+            'content': 'Hi 👋'
+        }
+    ]
+}
+
+print('=== Messenger ===')
+print('u.users')
+print('G.Groupes')
+print('x. Leave')
+choice = input('Select an option: ')
+if choice == 'x':
+    print('Bye!')
+elif choice == 'u':
+ for i in range (len(server['users'])):
+    print(server['users'][i]['name'],server['users'][i]['id'])
+elif choice== 'G':
+   for i in range (len(server['channels'])):
+    print(server['channels'][i]['name'],server['channels'][i]['member_ids'])
+
+else:
+    print('Unknown option:', choice)
